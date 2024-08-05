93faab42e7b9cbb523d0aae15a10bf0dbba243e0
Mohamad Alief Rizky Ramadhan <aliefm144@gmail.com>
2024-08-05 11:06:21 +0700

    make a 2d example game

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const scaleFactor = 4
+
+
+export const dialogueData = {
+    pc: `It's My PC really wanted to play Dragon Nest if I have time`,
+    "sofa-table": `A really nice sofa maybe I'll buy it so if my wife did not allow me to sleep on the bedroom I can sleep here`,
+    tv: 'Nice tv, can I watch netflix here? I guess so',
+    bed: `This bed is look good but I don't have wife to share the bed with. I must really find the woman to become my wife huh`,
+    resume: `This is my resume huh? I don't remember why I put this here`,
+    projects: `Theese are my projects on the past really miss to make one, maybe I'll make another one again.`,
+    library: `Here we go my manga collection, maybe I'll read it again sometimes`,
+    exit: `Have to go? click the close tab dude`,
+    'cs-degree': `It's my Computer Science Degree!! miss the good old time`
+}
